bb9ef7fcfe1d2c437f98aaa3513e593a1b687ff9
desolator645 <romanovivan67643@gmail.com>
2024-12-19 15:54:52 +0300

    №3
    2 задания

---
+++ #3.py
@@ -0,0 +1,15 @@
+class Employee:
+    name = None
+    age = None
+    salary = None
+    pass
+
+people = Employee()
+
+people.name = "Иван"
+people.age = 17
+people.salary = "500000"
+
+print(people.name)
+print(people.age)
+print(people.salary)
